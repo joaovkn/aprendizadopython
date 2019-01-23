8db5b55904c4ed793aac6c4295896d537056eed7
joaovkn <32307976+joaovkn@users.noreply.github.com>
2019-01-22 23:57:52 -0200

    Create aprendizado1.py

---
+++ aprendizado1.py
@@ -0,0 +1,45 @@
+print "hello, world"
+print "Water-there is not a drop of water there! Were Niagara but but a cataract of sand, would you travel your thousand miles to see it?"
+print("Hello World!")
+print("Deep into distant woodlands winds mazy way, reaching to overlapping spurs of mountains bathed in their hill-side blue.")
+print "This is a good string"
+print 'You can use single quotes or double quotes for a string'
+print "Mismatched quotes will cause a SyntaxError'
+print Without quotes will cause a NameError
+# This variable will be used to cound the numer of times anyone tweets the word persnickety
+persnickety_count = 0
+# This code will calculate the likelihood that it will rain tomorrow
+complicated_rain_calculation_for_tomorrow()
+# useful_value = old_sloppy_code()
+useful_value = new_clean_code()
+"""multi line string"""
+comandao = 'joaovkn'
+print(comandao)
+print("This message has mismatched quote marks!")
+print('Abracadabra')
+release_year = 2019
+runtime = 240
+rating_out_of_10 = 5.2
+print(25*68+13/28)
+quilt_width = 8
+quilt_length = 8
+print(quilt_width*quilt_length)
+print(6**2)
+print(7**2)
+print(8**2)
+a=6**2
+print(a**2)
+my_team = 27%4
+print(my_team)
+my_team = 26%4
+print(my_team)
+my_team = 28%4
+print(my_team)
+message = string1+string2+string3+string4+string5+string6
+total_price += nice_sweater
+total_price += fun_booksprint(message)
+my_age=24
+half_my_age=my_age/2
+greeting="Hello"
+name="Joao Victor"
+greeting_with_name=greeting+name
